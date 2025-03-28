bef47614e3fe5fa92e41e9bf67b715071e21a1a4
Till Brügmann <till.bruegmann@icloud.com>
2025-03-28 17:39:43 +0100

    Package Rename

--- Package.swift
+++ Package.swift
@@ -4,18 +4,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "Analytics",
+    name: "stoobit-analytics-swift",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Analytics",
-            targets: ["stoobit-analytics-swift"]),
+            targets: ["Analytics"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "stoobit-analytics-swift"
+            name: "Analytics"
         ),
     ]
 )
