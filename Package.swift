a3ec90a0453531c313b074e83e65357c1362b386
Till Brügmann <till.bruegmann@icloud.com>
2025-03-28 17:31:03 +0100

    Package Rename

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Analytics",
-            targets: ["Analytics"]),
+            targets: ["stoobit-analytics-swift"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
